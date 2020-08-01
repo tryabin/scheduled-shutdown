d3261f3e37639a3678fa559635a0846af1f17a32
Thomas Ryabin <thomas.ryabin@gmail.com>
2020-07-31 21:39:52 -0400

    Format and comment improvements

--- ScheduledShutdown.cs
+++ ScheduledShutdown.cs
@@ -8,29 +8,25 @@ namespace ScheduledShutdown {
 
             Dictionary<DayOfWeek, List<DateTime>> shutdownTimes = new Dictionary<DayOfWeek, List<DateTime>>();
             
-            //Load the shutdown times from the config file into the dictionary. 
-            //Multiple shutdown times in the same day can be done by separating the times with a comma (,).
+            // Load the shutdown times from the config file into the dictionary. 
+            // Multiple shutdown times in the same day can be done by separating the times with a comma.
             foreach (DayOfWeek dayOfWeek in Enum.GetValues(typeof(DayOfWeek))) {
-
+                // Get the setting for the current day of the week, if it exists.
                 String shutdownTimeParamValue = ConfigurationManager.AppSettings[dayOfWeek + "ShutdownTime"];
                 if (shutdownTimeParamValue == null) {
                     continue;
                 }
 
+                // Parse the setting value and add the times to the dictionary.
                 String[] shutdownTimeStringParts = shutdownTimeParamValue.Split(',');
-
                 List<DateTime> curSHutdownTimes = new List<DateTime>();
                 foreach (String shutdownTimeString in shutdownTimeStringParts) {
-
                     DateTime curShutdownTime = DateTime.ParseExact(shutdownTimeString, "hh:mm tt", null);
                     curSHutdownTimes.Add(curShutdownTime);
                 }
 
                 shutdownTimes.Add(dayOfWeek, curSHutdownTimes);
             }
-
-
-
 
 
             // Poll the time periodically. Shutdown if the previous time was before a shutdown time and the current
@@ -47,10 +43,8 @@ namespace ScheduledShutdown {
                 }
 
                 // Go through each shutdown time and see if it is between the previous time and the current time.
-                
                 foreach (DateTime shutdownTime in todaysShutdownTimes) {
                     if (shutdownTime > previousTime && shutdownTime <= currentTime) {
-                        System.Console.WriteLine("Shutting down...");
                         System.Diagnostics.Process.Start("shutdown", "/s /t 0");
                     }
                 }
